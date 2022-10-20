85c58b84f662e73ab67a81d3d46f7def1c69c800
riggja <soccerheadj@gmail.com>
2022-10-20 15:41:57 -0500

    adding code

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("EMRELEkPnwDNsuyHmYZYmMroJ69FpdAfSgDHrTxEYrq3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("5aM2Qt7xEs7Xf52JFM7xv72rYDBSV8tf56ht7zeB3XVa");
